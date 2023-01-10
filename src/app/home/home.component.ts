22100038faf9f2de28d4295bd55eb8f9637d6b5e
burakde2001 <burakde2001@gmail.com>
2023-01-10 22:02:49 +0100

    Minor add imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import log from 'loglevel';
 
@@ -27,7 +28,12 @@ import log from 'loglevel';
     templateUrl: './home.component.html',
     styleUrls: ['./home.scss'],
     standalone: true,
-    imports: [MatButtonModule, MatCardModule, MatFormFieldModule],
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+    ],
 })
 export class HomeComponent {
     constructor() {
