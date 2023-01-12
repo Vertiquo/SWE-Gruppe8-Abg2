9bd8951e999d7266b6e06d0c6ed30d8a4053a040
Justus Olischläger <Justus.Olischlaeger@gmx.de>
2023-01-12 09:30:00 +0100

    Button angepasst

--- src/app/kunde/details-kunde/details-bearbeiten.component.ts
+++ src/app/kunde/details-kunde/details-bearbeiten.component.ts
@@ -29,6 +29,7 @@ import log from 'loglevel';
 @Component({
     selector: 'hs-details-bearbeiten',
     templateUrl: './details-bearbeiten.component.html',
+    styleUrls: ['./details-kunde.component.scss'],
     imports: [
         MatButtonModule,
         MatCardModule,
